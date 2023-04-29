044cebed650318c79cb5437fc1ade980b36a691e
SantaRenata <renatasanta2002@gmail.com>
2023-04-29 21:01:56 +0200

    Listába iratás

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const[bikes, setBikes] = useState([]);
+
+  function getBikes() {
+    let host = 'http://localhost:8000/';
+    let endpoint = 'bikes'
+    let url = host + endpoint;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(result =>{
+      setBikes(result);
+    });
+  }
+
+  useEffect(()=>{
+    getBikes();
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Biciklik</Text>
+
+      <FlatList style={styles.fullList}
+      data={bikes}
+      renderItem = {({item}) => (
+        <Text style={styles.list}>Név: {item.name}  Ár: {item.price}</Text>
+      )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'beige',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    fontSize:28,
+    fontFamily:'Courier new'
+  },
+  list:{
+    padding:8,
+    fontSize:20
+  },
+  fullList:{
+    backgroundColor:'white'
+  }
 });
